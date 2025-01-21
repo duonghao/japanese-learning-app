ac1c07bbb6aaa1963f4d970054802e3bc2c5a6d4
Hao Duong <hao.duong11@gmail.com>
2025-01-21 20:32:07 +0900

    feat: add display of flashcards

--- src/components/flashcards.tsx
+++ src/components/flashcards.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import { Flashcard, getFlashcards } from "@/lib/firebase/firestore";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Flashcards() {
-  return <div>Hello</div>;
+  const [flashcards, setFlashcards] = useState<Flashcard[] | null>(null);
+
+  useEffect(() => {
+    const unsub = getFlashcards((flashcards) => setFlashcards(flashcards));
+
+    return unsub;
+  }, []);
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {flashcards?.map((flashcard) => (
+        <Card key={flashcard.word} className="w-48 h-32">
+          <CardHeader>
+            <CardTitle>{flashcard.word}</CardTitle>
+            <CardDescription>{flashcard.definition}</CardDescription>
+          </CardHeader>
+        </Card>
+      ))}
+    </div>
+  );
 }
--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -1,15 +1,26 @@
-import { addDoc, collection, Firestore } from "firebase/firestore";
+import { addDoc, collection, Firestore, onSnapshot } from "firebase/firestore";
+import { db } from "./clientApp";
 
-type FlashCard = {
+export type Flashcard = {
   word: string;
   definition: string;
 };
 
-export async function addFlashcard(db: Firestore, flashCard: FlashCard) {
+export async function addFlashcard(db: Firestore, flashCard: Flashcard) {
   try {
     const docRef = await addDoc(collection(db, "flashcards"), flashCard);
     console.log(`Flashcard written with id: ${docRef.id}`);
   } catch (error) {
     console.log("There was an error adding a flashcard.", error);
   }
+}
+
+export function getFlashcards(cb: (flashcard: Flashcard[]) => void) {
+  const unsub = onSnapshot(collection(db, "flashcards"), (collection) => {
+    const flashcards = collection.docs.map((doc) =>
+      doc.data(),
+    ) as unknown as Flashcard[];
+    cb(flashcards);
+  });
+  return unsub;
 }
