0274047f69d700e58ef6f322bb20fa7ff35d33b5
Hao Duong <hao.duong11@gmail.com>
2025-02-04 21:18:42 +0900

    feat: add filtering on deck name

--- src/app/decks/data-grid.tsx
+++ src/app/decks/data-grid.tsx
@@ -2,7 +2,9 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -16,6 +18,8 @@ import {
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 interface DataGridProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -26,32 +30,50 @@ export function DataGrid<TData, TValue>({
   columns,
   data,
 }: DataGridProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   });
 
   return (
-    <div className="rounded-md border grid grid-flow-col gap-4 grid-cols-5 p-4">
-      {table.getRowModel().rows?.length ? (
-        table.getRowModel().rows.map((row) => (
-          <Card className="w-[250px]" key={row.id}>
-            <CardHeader>
-              <CardTitle>{row.getValue("name")}</CardTitle>
-              <CardDescription>{row.getValue("description")}</CardDescription>
-            </CardHeader>
-            <CardFooter className="flex justify-between">
-              <Button variant="destructive">Delete</Button>
-              <Button asChild>
-                <Link href={`/decks/${row.getValue("id")}`}>View</Link>
-              </Button>
-            </CardFooter>
-          </Card>
-        ))
-      ) : (
-        <div className="h-24 text-center">No results.</div>
-      )}
+    <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter by name..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border grid grid-flow-col gap-4 grid-cols-5 p-4">
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <Card className="w-[250px]" key={row.id}>
+              <CardHeader>
+                <CardTitle>{row.getValue("name")}</CardTitle>
+                <CardDescription>{row.getValue("description")}</CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-between">
+                <Button variant="destructive">Delete</Button>
+                <Button asChild>
+                  <Link href={`/decks/${row.getValue("id")}`}>View</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))
+        ) : (
+          <div className="h-24 text-center">No results.</div>
+        )}
+      </div>
     </div>
   );
 }
